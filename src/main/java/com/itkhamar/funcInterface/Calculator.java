9587852809d878e7c28824d740746ad1dab9395d
absiddik <absiddika@gmail.com>
2025-09-28 01:37:51 +0600

    Functional Interface with lambda

---
+++ src/main/java/com/itkhamar/funcInterface/Calculator.java
@@ -0,0 +1,6 @@
+package com.itkhamar.funcInterface;
+
+@FunctionalInterface
+public interface Calculator {
+    int calculate(int a, int b);
+}
--- src/main/java/com/itkhamar/funcInterface/Tester.java
+++ src/main/java/com/itkhamar/funcInterface/Tester.java
@@ -19,5 +19,15 @@ public class Tester {
         // This is lambda expression with Functional Interface
         Greeting greeting3 = () -> System.out.println("Hello World!");
         greeting3.greet();
+
+        // another lambda expression with functional interface
+        Calculator addition = (int a, int b) -> a + b;
+        System.out.println("addition: " + addition.calculate(10, 20));
+        Calculator subtraction = (int a, int b) -> a - b;
+        System.out.println("subtraction: " + subtraction.calculate(20, 5));
+        Calculator multiplication = (int a, int b) -> a * b;
+        System.out.println("multiplication: " + multiplication.calculate(10, 20));
+        Calculator division = (int a, int b) -> a / b;
+        System.out.println("division: " + division.calculate(20, 5));
     }
 }
